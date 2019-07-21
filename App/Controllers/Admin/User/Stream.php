a7cd8c46fa6680490ed3a6d287b881326081f347
uki7991 <tilek.kubanov@gmail.com>
2019-07-22 03:34:05 +0600

    add action

--- App/Controllers/Admin/User/Stream.php
+++ App/Controllers/Admin/User/Stream.php
@@ -12,6 +12,10 @@ namespace App\Controllers\Admin\User;
 use App\Controllers\Admin\Base;
 use Core\CustomException;
 use Core\View;
+use Models\BranchQuery;
+use Models\ConfigQuery;
+use Models\CourseStreamStatusQuery;
+use Models\CurrencyQuery;
 use Models\UserQuery;
 
 class Stream extends Base
@@ -47,5 +51,48 @@ class Stream extends Base
             );
             View::renderTemplate('Admin/errorPage.html', $this->data);
         }
+    }
+
+    public function addAction()
+    {
+        try {
+            $this->helper->shouldHavePrivilege('COURSE_STREAM_ADMIN');
+
+            $userId = (isset($this->params['userid']) ? $this->params['userid'] : null);
+            if (is_null($userId) || intval($userId) == 0){
+                throw new CustomException("ID пользователя не был указан", 403);
+            }
+
+            $user = UserQuery::create()->findPk(intval($userId));
+            if (is_null($user)){
+                throw new CustomException("Курс не найден", 404);
+            }
+            $branches = BranchQuery::create()->find();
+            $statuses = CourseStreamStatusQuery::create()->find();
+            $currencies = CurrencyQuery::create()->find();
+
+            $instructorGroup = ConfigQuery::create()->findOneByKey('default_instructor_group');
+            $instructors = UserQuery::create()->filterByCurrentGroupId(intval($instructorGroup->getValue()))->find();
+
+            $this->data = array_merge( $this->data,
+                array(
+                    'student' => $user,
+                    'branches' => $branches,
+                    'statuses' => $statuses,
+                    'instructors' => $instructors,
+                    'currencies' => $currencies
+                )
+            );
+            View::renderTemplate('Admin/User/Stream/add.html', $this->data);
+        } catch (CustomException $e) {
+            $this->data = array_merge( $this->data,
+                array(  'error_code' => $e->getCode(),
+                    'error_title' => "Ошибка",
+                    'error_message' => $e->getMessage(),
+                )
+            );
+            View::renderTemplate('Admin/errorPage.html', $this->data);
+        }
+
     }
 }
